a4d16e5dd7e27b7805f2a5a9037391e5b8e526d1
Abdelazizmostafa <abdoelflal93@gmail.com>
2019-09-29 15:19:42 +0200

    Js File Modified

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@
   var siteName = document.getElementById('siteName').value;
   var siteUrl  = document.getElementById('siteUrl').value;
   
+  if(!validateForm(siteName, siteUrl)){
+    return false;
+  }
   var Bookmark = {
     name:siteName,
     url :siteUrl
@@ -30,10 +33,47 @@
       //re-set to localStorage
       localStorage.setItem('bookmarks' , JSON.stringify(bookmarks));
     }
- 
+
+    //Clear  form
+    document.getElementById('myFrom').reset();
+
+    //re-fetch bookmarks
+    fetchBookmarks();
    // prevent form from submitting
    e.preventDefault();
   }
+
+  
+
+  //delete Bookmark Function 
+    function deleteBookmark(url){
+    // Get bookmarks from localStorage
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    // Loop through the bookmarks
+    for(var i =0;i < bookmarks.length;i++){
+        if(bookmarks[i].url == url){
+        // Remove from array
+        bookmarks.splice(i, 1);
+        }
+    }
+    // Re-set back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+    // Re-fetch bookmarks
+    fetchBookmarks();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
   //feteching bookmarks
   function fetchBookmarks(){
     //get bookmarks from localStorage  
@@ -50,9 +90,34 @@
 
           bookmarksResult.innerHTML +=  '<div  class="well">'+
                                         '<h3>'+name+'  '+
-                                 '<a class="btn btn-default  target="_blank" href="'+url+'">Visit</a>' 
+                         '<a class="btn btn-default"  target="_blank" href="'+url+'">Visit</a>'+' '+
+                         '<a  onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
                                         '</h3>'+
                                         '</div>';
      }
 
+  }
+// Validate Form
+function validateForm(siteName, siteUrl){
+    if(!siteName || !siteUrl){
+      alert('Please fill in the form');
+      return false;
+    }
+  
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+  
+    if(!siteUrl.match(regex)){
+      alert('Please use a valid URL');
+      return false;
+    }
+  
+    return true;
+  }
+  
+  function addhttp(url) {
+    if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
+        url = "http://" + url;
+    }
+    return url;
   }
